33802fbdecf929f0c9130d3e608ab9fe8a4ecc52
chrelmi <christian.elmi@gmail.com>
2019-04-10 16:35:27 +0200

    graphic version

--- pich.py
+++ pich.py
@@ -80,10 +80,10 @@ def update(file):
 	l4.grid(row=0,column=0, padx=10)
 	l5=tkinter.Label(root,text="Squadra Ospite",bg="#000000",fg="#FFFFFF")
 	l5.config(font=("Noto Sans Mono CJK JP Bold", 24))
-	l5.grid(row=1,column=0, padx=10)	
-	e1.grid(row=0,column=4)	
+	l5.grid(row=1,column=0, padx=10)
+	e1.grid(row=0,column=4)
 	e2.grid(row=1,column=4)
-	e3.grid(row=2,column=4)	
+	e3.grid(row=2,column=4)
 	e4.grid(row=0,column=1) #Squadra Casa
 	e5.grid(row=1,column=1) #squadra Trasferta
 	b=Button(root, text="Invia", width=10, command=lambda:callback(file,squadracs,squadratr))
@@ -147,9 +147,9 @@ def callback(file,squadracs,squadratr):
 					if goltr<golcs:
 						countptr=countptr+1
 					if goltr>golcs:
-						countvtr=countvtr+1	
+						countvtr=countvtr+1
 	print("----------------------------------------------------------------------------------------------")
-	#calcolo quote reali	
+	#calcolo quote reali
 	uno=e1.get() #input("Quota 1: ") #e1
 	uno=float(uno)
 	x=e2.get() #input("Quota x: ") #e2
@@ -172,6 +172,15 @@ def callback(file,squadracs,squadratr):
 	uno=str(uno)
 	x=str(x)
 	due=str(due)
+	luno=tkinter.Label(root,text="Quota 1\n"+uno,bg="#000000",fg="#FFFFFF")
+	lx=tkinter.Label(root,text="Quota X\n"+x,bg="#000000",fg="#FFFFFF")
+	ldue=tkinter.Label(root,text="Quota 2\n"+due,bg="#000000",fg="#FFFFFF")
+	luno.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	lx.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	ldue.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	luno.grid(row=4,column=0,padx=5)
+	lx.grid(row=4,column=1)
+	ldue.grid(row=4,column=3,columnspan=2)
 	print("Quote reali:	1	X	2")
 	print("               "+uno+"   "+x+"    "+due)
 	print("----------------------------------------------------------------------------------------------")
@@ -181,6 +190,12 @@ def callback(file,squadracs,squadratr):
 	countcs=str(countcs)
 	countvc=str(countvc)
 	countpcs=str(countpcs)
+	storico1=tkinter.Label(root,text="Partite totali in casa "+squadracs+": "+"\nPartite vinte in casa "+squadracs+": "+"\nPartite perse in casa "+squadracs+": ",bg="#000000",fg="#FFFFFF")
+	storico1.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	storico1.grid(row=5,column=0,columnspan=2,sticky=W)
+	ris1=tkinter.Label(root,text=countcs+"\n"+countvc+"\n"+countpcs,bg="#000000",fg="#FFFFFF")
+	ris1.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	ris1.grid(row=5,column=2,sticky=W)
 	print("Partite totali in casa "+squadracs+": "+countcs+"\nPartite vinte in casa "+squadracs+": "+countvc+"\nPartite perse in casa: "+countpcs)
 	tr=countr
 	ptr=countptr
@@ -188,6 +203,12 @@ def callback(file,squadracs,squadratr):
 	countr=str(countr)
 	countvtr=str(countvtr)
 	countptr=str(countptr)
+	storico2=tkinter.Label(root,text="Partite totali in trasferta "+squadratr+": "+"\nPartite vinte in trasferta "+squadratr+": "+"\nPartite perse in trasferta "+squadratr+": ",bg="#000000",fg="#FFFFFF")
+	storico2.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	storico2.grid(row=6,column=0,columnspan=2,sticky=W,pady=10)
+	ris2=tkinter.Label(root,text=countr+"\n"+countvtr+"\n"+countptr,bg="#000000",fg="#FFFFFF")
+	ris2.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	ris2.grid(row=6,column=2,sticky=W)
 	print("\n\nPartite totali in trasferta "+squadratr+": "+countr+"\nVittorie perse in trasferta "+squadratr+": "+countptr+"\nPartite vinte in trasferta: "+countvtr)
 	#calcolo picchetti
 	picchettocs=(vc+ptr)/(cs+tr)
@@ -212,12 +233,21 @@ def callback(file,squadracs,squadratr):
 	picchettox=str(picchettox)
 	picchettocs=str(picchettocs)
 	picchettotr=str(picchettotr)
+	luno1=tkinter.Label(root,text="Quota 1\n"+picchettocs+"%"+"\n"+quotacasa,bg="#000000",fg="#FFFFFF")
+	lx1=tkinter.Label(root,text="Quota X\n"+picchettox+"%"+"\n"+quotax,bg="#000000",fg="#FFFFFF")
+	ldue1=tkinter.Label(root,text="Quota 2\n"+picchettotr+"%"+"\n"+quotatr,bg="#000000",fg="#FFFFFF")
+	luno1.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	lx1.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	ldue1.config(font=("Noto Sans Mono CJK JP Bold", 16))
+	luno1.grid(row=7,column=0,padx=5)
+	lx1.grid(row=7,column=1)
+	ldue1.grid(row=7,column=3,columnspan=2)
 	print("Pic 1: "+picchettocs+"%"+"\nPic X: "+picchettox+"%"+"\nPic 2: "+picchettotr+"%")
 	print("QUOTE FINALI:     1       X       2")
 	print("                "+quotacasa+"    "+quotax+"     "+quotatr)
 #Definizione grafica
 root=Tk()
-root.geometry("800x400")
+root.geometry("800x600")
 root.title("Calcolatore Picchetto Tecnico")
 menu=Menu(root)
 root.config(menu=menu)
